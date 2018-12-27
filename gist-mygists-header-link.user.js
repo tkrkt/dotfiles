5f743cf686b2a57ed2411fb26db2df37491b138d
tkrkt <tkrkt2773@gmail.com>
2018-12-28 00:20:21 +0900

    Update gist-mygists-header-link.user.js

--- gist-mygists-header-link.user.js
+++ gist-mygists-header-link.user.js
@@ -10,16 +10,14 @@
 
 const meta = document.querySelector('meta[name=user-login]');
 
-if (meta){
+if (meta) {
   const user = meta.getAttribute('content');
   const container = document.querySelector('[role=navigation]');
-  
   const li = document.createElement('li');
   const a = document.createElement('a');
   a.setAttribute('class', 'HeaderNavlink px-2');
   a.setAttribute('href', '/' + user);
   a.textContent = 'My Gists';
   li.appendChild(a);
-  
   container.insertBefore(li, container.firstChild);
 }
