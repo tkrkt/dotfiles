7aa3df810f4e13d10bf20d574f33034292cce23b
tkrkt <tkrkt2773@gmail.com>
2018-12-28 00:17:13 +0900

    Create gist-mygists-header-link.user.js

---
+++ gist-mygists-header-link.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        MyGists Header Link
+// @description Add "MyGists" link into header
+// @namespace   https://github.com/tkrkt/userscripts
+// @include     https://gist.github.com/*
+// @version     3
+// @grant       none
+// ==/UserScript==
+
+const meta = document.querySelector('meta[name=user-login]');
+
+if (meta){
+  const user = meta.getAttribute('content');
+  const container = document.querySelector('[role=navigation]');
+  
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.setAttribute('class', 'HeaderNavlink px-2');
+  a.setAttribute('href', '/' + user);
+  a.textContent = 'My Gists';
+  li.appendChild(a);
+  
+  container.insertBefore(li, container.firstChild);
+}
