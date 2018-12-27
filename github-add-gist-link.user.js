3dcf5ede27ae689650879af1bfa84fe80a59a42c
tkrkt <tkrkt2773@gmail.com>
2018-12-28 00:27:41 +0900

    Create github-add-gist-link.user.js

---
+++ github-add-gist-link.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        [GitHub] Add gist link
+// @description Add Gist link into user profile page
+// @namespace   https://github.com/tkrkt/userscripts
+// @include     https://github.com/*
+// @exclude     https://github.com/*/*
+// @author      tkrkt
+// @version     3
+// @grant       none
+// ==/UserScript==
+
+const text = `View ${location.pathname.slice(1)} on Gist`;
+const a = document.createElement("a");
+
+a.classList.add("btn", "btn-block");
+a.style.marginTop = "10px";
+a.href = `https://gist.github.com${location.pathname}`
+a.title = text;
+a.textContent = text;
+
+const container = document.querySelector('.user-profile-following-container');
+container.appendChild(a);
